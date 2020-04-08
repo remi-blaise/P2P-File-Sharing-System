805da5dc76834943f7b8d50b2ad72cc9689be7dc
Rémi Blaise <pro@remi-blaise.com>
2020-04-08 23:21:19 +0200

    Choose strategy

--- peer/src/client.js
+++ peer/src/client.js
@@ -75,7 +75,7 @@ function start() {
 					file.save()
 
 					// Invalidate request
-					invalidate(await generateMessageID(), file.name, file.version)
+					if (config.strategy === 0) invalidate(await generateMessageID(), file.name, file.version)
 				} else {
 					File.create({ name: filename, owned: true })
 						.catch(err => console.error(err))
